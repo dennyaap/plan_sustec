4521855ab9eb1e785bc644923e7d37ceecc5c17d
Denis Alekhin <alekhin.den69@gmail.com>
2022-06-06 22:48:24 +0500

    added project context

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ProjectStore from './store/ProjectStore';
 import UserStore from './store/UserStore';
 
 const Context = createContext(null);
@@ -8,7 +9,8 @@ const Context = createContext(null);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Context.Provider value={{
-        user: new UserStore()
+        user: new UserStore(),
+		project: new ProjectStore()
     }}
     >
         <App />
