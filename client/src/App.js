d89d7259668fdd3f4f5a6b45bf289b2f5600904c
Denis Alekhin <alekhin.den69@gmail.com>
2022-06-06 22:40:24 +0500

    added loader spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
+import { useState, useContext, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 import { observer } from 'mobx-react-lite';
+import Context from './index';
+import { check } from './http/userAPI';
+import Spinner from 'react-bootstrap/Spinner'
 
 const App = observer( () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </div>
-  );
+	const { user } = useContext( Context );
+	const [ loading, setLoading ] = useState(true);
+
+	useEffect( () => {
+		setTimeout( () => {
+			check().then( data => {
+				user.setUser(true);
+				user.setIsAuth(true);
+			}).finally( () => setLoading( false ) )
+		}, 1000);
+	}, []);
+
+	if( loading ) {
+		return <Spinner 
+			animation={ 'border' } 
+			variant={ 'primary' }
+			style={ { position: "absolute", top: "50%", left: "50%", width: 120, height: 120 } }
+		/>
+	}
+
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<AppRouter />
+			</BrowserRouter>
+		</div>
+	);
 });
 
 export default App;
