0760f0b843959e76e70be78e0c0334fc66f0e495
Denis Alekhin <alekhin.den69@gmail.com>
2022-06-06 22:45:57 +0500

    added fetchProjects

--- client/src/pages/projects/Projects.jsx
+++ client/src/pages/projects/Projects.jsx
@@ -1,16 +1,24 @@
+import { useContext, useEffect } from 'react';
 import ProjectList from "../../components/projectlist/ProjectList";
+import { fetchProjects } from '../../http/projectAPI';
+import Context from '../../index';
 
 const Projects = () => {
-	const projects = [
-		{
-			name: 'Проект 1',
-			createdAt: '6.06.22',
-			status: 'Активен'
-		}
-	];
+	// const projects = [
+	// 	{
+	// 		name: 'Проект 1',
+	// 		createdAt: '6.06.22',
+	// 		status: 'Активен'
+	// 	}
+	// ];
+	const { project } = useContext(Context);
+
+	useEffect( () => {
+		fetchProjects().then( data => project.setProjects(data));
+	}, []);
 
   	return (
-    	<ProjectList projects={ projects }/>
+    	<ProjectList />
   	);
 }
 
