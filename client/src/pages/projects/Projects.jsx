99a0f7190a5ca6b46fe7e814cca2d90d6c1483d8
Denis Alekhin <alekhin.den69@gmail.com>
2022-06-10 12:50:36 +0500

    added spinner loader

--- client/src/pages/projects/Projects.jsx
+++ client/src/pages/projects/Projects.jsx
@@ -1,15 +1,28 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ProjectList from "../../components/projectlist/ProjectList";
 import { fetchProjects } from '../../http/projectAPI';
 import { observer } from 'mobx-react-lite';
 import Context from '../../index';
+import Spinner from 'react-bootstrap/Spinner'
+
 
 const Projects = observer (() => {
 	const { project, user } = useContext(Context);
+	const [loading, setLoading ] = useState( true );
 
 	useEffect( () => {
-		fetchProjects(user.currentUser.id).then( data => project.setProjects( data.rows ));
-	});
+		fetchProjects(user.currentUser.id).then( data => 
+			project.setProjects( data.rows )
+		).finally( () => setLoading( false ));
+	}, [] );
+	
+	if( loading ) {
+		return <Spinner 
+			animation={ 'border' } 
+			variant={ 'primary' }
+			style={ { position: "absolute", top: "50%", left: "50%", width: 120, height: 120 } }
+		/>
+	}
 
   	return (
     	<ProjectList />
