19e48fbb9c0dd8ff3399a5425f4f8812a9d143cb
Denis Alekhin <alekhin.den69@gmail.com>
2022-06-10 21:09:48 +0500

    fix: rename

--- client/src/store/ProjectStore.js
+++ client/src/store/ProjectStore.js
@@ -3,14 +3,21 @@ import { makeAutoObservable } from 'mobx';
 export default class ProjectStore {
     constructor() {
         this._projects = [];
+		this._statuses = [];
         makeAutoObservable(this);
     }
 
     setProjects(projects) {
 		this._projects = projects;
 	}
+	setStatuses(statuses) {
+		this._statuses = statuses;
+	}
 
 	get projects() {
 		return this._projects;
+	}
+	get statuses() {
+		return this._statuses;
 	}
 }
