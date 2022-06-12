61f07226fbdea469e16f5a338803bb171ae68138
Mihastick <savilov990@gmail.com>
2022-06-12 22:41:40 +0500

    added avatar for user

--- client/src/components/drawer/Drawer.jsx
+++ client/src/components/drawer/Drawer.jsx
@@ -15,6 +15,9 @@ import TaskIcon from '@mui/icons-material/Task';
 import PersonIcon from '@mui/icons-material/Person';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { Box } from '@mui/system';
+import { Avatar } from '@mui/material';
+import { Typography } from '@mui/material';
 
 import { NavLink } from 'react-router-dom';
 import { PROJECTS_ROUTE, TASKS_ROUTE } from '../../consts/consts';
@@ -30,156 +33,181 @@ export const DrawerHeader = styled('div')(({ theme }) => ({
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-  }));
+}));
 
 const openedMixin = (theme) => ({
     width: drawerWidth,
     transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
     }),
     overflowX: 'hidden',
-  });
-  
-  const closedMixin = (theme) => ({
+});
+
+const closedMixin = (theme) => ({
     transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: 'hidden',
     width: `calc(${theme.spacing(7)} + 1px)`,
     [theme.breakpoints.up('sm')]: {
-      width: `calc(${theme.spacing(8)} + 1px)`,
+        width: `calc(${theme.spacing(8)} + 1px)`,
     },
-  });
+});
 
 const DrawerStyle = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
     ({ theme, open }) => ({
-      width: drawerWidth,
-      flexShrink: 0,
-      whiteSpace: 'nowrap',
-      boxSizing: 'border-box',
-      ...(open && {
-        ...openedMixin(theme),
-        '& .MuiDrawer-paper': openedMixin(theme),
-      }),
-      ...(!open && {
-        ...closedMixin(theme),
-        '& .MuiDrawer-paper': closedMixin(theme),
-      }),
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
     }),
-  );
+);
 
 const Drawer = ({ open, handleDrawerClose }) => {
     const theme = useTheme();
-	const location = useLocation();
+    const location = useLocation();
 
     return (
         <DrawerStyle variant="permanent" open={open}>
-                <DrawerHeader>
+            <DrawerHeader>
                 <IconButton onClick={handleDrawerClose}>
                     {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
                 </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <List>
+            </DrawerHeader>
+            <Divider />
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: '20px',
+                marginBottom: '20px',
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.0,
+            }}>
+                <Avatar
+                    src='https://sun9-84.userapi.com/s/v1/if2/oDsveymj8dNLm6BKVaIAs3EhfwpGUD92PPQwFbK2RT_AiwjZNaahjQpzbAOmWl_C4Shi70zmmzKoJjsZ3UHyDxS7.jpg?size=810x1080&quality=95&type=album' 
+                    sx={{
+                        width: '32px', height: '32px', bgcolor: '#4287f5', minWidth: 0,
+                        mr: open ? 3 : 'auto',
+                        justifyContent: 'center',
+                        marginLeft:open ?'0px':'62px'
+                    }}>
+                    Da
+                </Avatar>
+                <Box sx={{ opacity: open ? 1 : 0 }}>
+                    <Typography>Дениска</Typography>
+                    <Typography sx={{ fontSize: '8pt', color: 'gray' }}>Dennya</Typography>
+                </Box>
+            </Box>
+            <List>
                 {[
                     {
                         name: 'Мои проекты',
-                        icon: <BusinessCenterIcon/>,
-						route: PROJECTS_ROUTE
-                        }, 
+                        icon: <BusinessCenterIcon />,
+                        route: PROJECTS_ROUTE
+                    },
                     {
                         name: 'Мои задачи',
-                        icon: <TaskIcon/>,
-						route: TASKS_ROUTE
-                        }, 
+                        icon: <TaskIcon />,
+                        route: TASKS_ROUTE
+                    },
                     {
                         name: 'Сотрудники',
-                        icon: <PersonIcon/>,
-						route: TASKS_ROUTE
-                        }
-                    ].map(({name, icon, route}) => (
+                        icon: <PersonIcon />,
+                        route: TASKS_ROUTE
+                    }
+                ].map(({ name, icon, route }) => (
                     <ListItem key={name} disablePadding sx={{ display: 'block' }}>
-                    <NavLink to={route} style={ { textDecoration: 'none' } }><ListItemButton
-                        sx={{
-                        minHeight: 48,
-                        justifyContent: open ? 'initial' : 'center',
-                        px: 2.5,
-						color: location.pathname === route ? '#109CF1' : '#334D6E'
-                        }}
-                    >
-                        <ListItemIcon
-                        sx={{
-                            minWidth: 0,
-                            mr: open ? 3 : 'auto',
-                            justifyContent: 'center',
-							color: location.pathname === route ? '#109CF1' : '#C2CFE0'
-                        	}}
+                        <NavLink to={route} style={{ textDecoration: 'none' }}><ListItemButton
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? 'initial' : 'center',
+                                px: 2.5,
+                                color: location.pathname === route ? '#109CF1' : '#334D6E'
+                            }}
                         >
-                        	{ icon }
-                        </ListItemIcon>
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : 'auto',
+                                    justifyContent: 'center',
+                                    color: location.pathname === route ? '#109CF1' : '#C2CFE0'
+                                }}
+                            >
+                                {icon}
+                            </ListItemIcon>
 
-                        <ListItemText 
-							primary={ name } 
-							sx={ { opacity: open ? 1 : 0 } } 
-						/>
-						
-                    </ListItemButton>
-					</NavLink>
+                            <ListItemText
+                                primary={name}
+                                sx={{ opacity: open ? 1 : 0 }}
+                            />
+
+                        </ListItemButton>
+                        </NavLink>
                     </ListItem>
                 ))}
-                </List>
-                <Divider />
-                <List>
-                    <ListItem key={'Настройки'} disablePadding sx={{ display: 'block' }}>
+            </List>
+            <Divider />
+            <List>
+                <ListItem key={'Настройки'} disablePadding sx={{ display: 'block' }}>
                     <ListItemButton
                         sx={{
-                        minHeight: 48,
-                        justifyContent: open ? 'initial' : 'center',
-                        px: 2.5,
+                            minHeight: 48,
+                            justifyContent: open ? 'initial' : 'center',
+                            px: 2.5,
                         }}
                     >
                         <ListItemIcon
-                        sx={{
-                            minWidth: 0,
-                            mr: open ? 3 : 'auto',
-                            justifyContent: 'center',
-                        }}
+                            sx={{
+                                minWidth: 0,
+                                mr: open ? 3 : 'auto',
+                                justifyContent: 'center',
+                            }}
                         >
-                        <SettingsIcon />
+                            <SettingsIcon />
                         </ListItemIcon>
                         <ListItemText primary={'Настройки'} sx={{ opacity: open ? 1 : 0 }} />
                     </ListItemButton>
-                    </ListItem>
-                </List>
-                
-                <List>
-                
-                    <ListItem key={'Выйти'} disablePadding sx={{ display: 'block',position:'absolute',top:260}}>
+                </ListItem>
+            </List>
+
+            <List>
+
+                <ListItem key={'Выйти'} disablePadding sx={{ display: 'block', position: 'absolute', top: 180 }}>
                     <ListItemButton
                         sx={{
-                        minHeight: 48,
-                        justifyContent: open ? 'initial' : 'center',
-                        px: 2.5,
+                            minHeight: 48,
+                            justifyContent: open ? 'initial' : 'center',
+                            px: 2.5,
                         }}
                     >
                         <ListItemIcon
-                        sx={{
-                            minWidth: 0,
-                            mr: open ? 3 : 'auto',
-                            justifyContent: 'center',
-							color: '#FE5050'
-                        }}
+                            sx={{
+                                minWidth: 0,
+                                mr: open ? 3 : 'auto',
+                                justifyContent: 'center',
+                                color: '#FE5050'
+                            }}
                         >
-                        <LogoutIcon/>
+                            <LogoutIcon />
                         </ListItemIcon>
                         <ListItemText primary={'Выйти'} sx={{ opacity: open ? 1 : 0, color: '#FE5050' }} />
                     </ListItemButton>
-                    </ListItem>
-                    <DrawerHeader />
-                </List>
-            </DrawerStyle>
+                </ListItem>
+                <DrawerHeader />
+            </List>
+        </DrawerStyle>
     );
 }
 
