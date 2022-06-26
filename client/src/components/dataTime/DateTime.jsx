719706ca21d27f8ea4ff97b61245043561439235
Mihastick <savilov990@gmail.com>
2022-06-27 01:16:40 +0500

    editing styled dateTimePicker

--- client/src/components/dataTime/DateTime.jsx
+++ client/src/components/dataTime/DateTime.jsx
@@ -3,33 +3,56 @@ import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import EventIcon from '@mui/icons-material/Event';
 import ru from "date-fns/locale/ru";
 import './style.css';
 
 
 export default function DateTime() {
-    const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
-  
-    const handleChange = (newValue) => {
-      setValue(newValue);
-    };
-  
-    return (
-      <LocalizationProvider dateAdapter={AdapterDateFns} locale={ru}>
-          <DateTimePicker
-            value={value}
-            id='dtp'
-            onChange={handleChange}
-            renderInput={(params) => <TextField {...params}
-            sx={{
-              width:190,
-              backgroundColor:'#109CF1',
-              borderRadius:20,
-            }}
-             />
+  const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ru} id='dtp'>
+      <DateTimePicker
+        value={value}
+        open={open}
+        onOpen={handleOpen}
+        onClose={handleClose}
+        onChange={handleChange}
+        renderInput={(params) => <TextField
+        id='ddd'
+          sx={{
+            width:'200px',
+            borderRadius:20,
+            backgroundColor:'#109CF1',
+            fontSize:'14px'
+          }}
+          {...params}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton edge="end" onClick={handleOpen}>
+                  <EventIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          
+        />
         }
-          />
-      </LocalizationProvider>
-    );
-  }
+      />
+    </LocalizationProvider>
+  );
+}
 
